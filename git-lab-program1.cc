af84fb911016bfb03c3e4ef54c74845c34580f70
joey Valentino <jv952522@ohio.edu>
2024-01-26 15:31:52 -0500

    M

---
+++ git-lab-program1.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Joey Valentino
+ *        Date: 1/26/24
+ * Description: to say hello git 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+cout << "Hello Git!!" << endl;
+    return 0;
+}// main
